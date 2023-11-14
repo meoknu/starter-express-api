4ff9b4f65435b41ab16be97f06198a70c029fb9a
Naveen Raghuvanshi <naveen@olecons.com>
2023-11-14 14:42:50 +0530

    update

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/req', (req, res) => {
+    res.send('Response from web server!')
+})
 app.listen(process.env.PORT || 3000)
