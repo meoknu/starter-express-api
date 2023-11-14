6598f2f943c4669523df418c8cd6bec87c99d9f8
Naveen Raghuvanshi <naveen@olecons.com>
2023-11-14 14:54:47 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const app = express()
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
